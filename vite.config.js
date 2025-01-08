d962b85c53f4424fb673731037b7b0562a22a9fa
Jason Ford <fordjason@gmail.com>
2025-01-08 11:39:06 -0800

    First commit

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 5112,
+  },
+  build: {
+    target: 'esnext'
+  },
+  plugins: [
+    vue(),
+    basicSsl()
+  ]
+})
