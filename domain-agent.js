47bde84a6c5f1b8f2cd3287495536e4ad7f35a97
Jason Ford <fordjason@gmail.com>
2025-01-13 16:01:09 -0800

    Update domain agent

--- domain-agent.js
+++ domain-agent.js
@@ -1,7 +1,9 @@
   import Agent from 'npm:@knowlearning/agents/deno.js'
 
-  const TRANSLATION_TYPE = 'application/json;type=xapi-statement'
-  const TRANSLATABLE_TARGET_TYPE = 'application/json;type=translatable_target'
+  const XAPI_TYPE = 'application/json;type=xapi-statement'
+  const RESULT_TYPE = 'application/json;type=xapi-result'
+  const CONTEXT_TYPE = 'application/json;type=xapi-context'
+  const AUTHORITY_TYPE = 'application/json;type=xapi-authority'
 
   Agent.on('child', child => {
     const { environment: { user } } = child
@@ -9,90 +11,34 @@
 
     child.on('mutate', async ({ scope, id, patch }) => {
       if (scope.startsWith('xapi/')) {
-        //  TODO: validate user is a domain agent with rights to set
-        const  { source_language, source_string } = await Agent.state(id)
-        await setTranslation(id, source_language, source_string)
-      }
-      else if (scope.startsWith('translations/')) {
-        const [,, language] = scope.split('/')
+        const { origin, verb, object, result, context, authority } = await Agent.state(id)
+        const actor = await Agent.metadata(origin).then(md => md.owner)
 
-        const paths = await Agent.state(id)
-        Agent.log('TRANSLATIONS PATCH', patch)
+        const active = { origin, verb, object }
 
-        const { op, path: [translatable_target], value } = patch[0]
-
-        if (op === 'add' || op === 'replace') {
-          setTranslation(translatable_target, language, value)
+        if (result) {
+          active.result = await Agent.create({
+            active_type: RESULT_TYPE,
+            active: result
+          })
         }
-        else if (op === 'remove') {
-          setTranslation(translatable_target, language, null)
+        if (context) {
+          active.context = await Agent.create({
+            active_type: CONTEXT_TYPE,
+            active: context
+          })
         }
-      }
-      else if (await isTranslatableItem(id)) {
-        await handleTranslatableItem(id)
+        if (authority) {
+          active.authority = await Agent.create({
+            active_type: AUTHORITY_TYPE,
+            active: authority
+          })
+        }
+
+        await Agent.create({
+          active_type: XAPI_TYPE,
+          active
+        })
       }
     })
   })
-
-
-  async function setTranslation(id, language, string) {
-    const scope = await getTranslationScope(id, language)
-
-    scope.translatable_target = id
-    scope.language = language
-    scope.value = string
-  }
-
-  async function getTranslationScope(id, lang) {
-    const name = `translation/${id}/${lang}`
-    const metadata = await Agent.metadata(name)
-
-    if (metadata.active_type !== TRANSLATION_TYPE) metadata.active_type = TRANSLATION_TYPE
-
-    return Agent.state(name)
-  }
-
-  function resolvePath(path, value) {
-    while (path.length && value) value = value[path.shift()]
-    return value
-  }
-
-  async function handleTranslatableItem(id) {
-    const itemState = await Agent.state(id)
-    await Promise.all(itemState.translations.paths.map(async path => {
-      const translatableTargetName = `translatable_target/${JSON.stringify([id, ...path])}`
-      Agent.log('GETTING METADATA', translatableTargetName)
-      const translatableTargetMetadata = await Agent.metadata(translatableTargetName)
-      Agent.log('GOT METADATA', JSON.parse(JSON.stringify(translatableTargetMetadata)))
-
-      if (translatableTargetMetadata.active_type !== TRANSLATABLE_TARGET_TYPE) {
-        translatableTargetMetadata.active_type = TRANSLATABLE_TARGET_TYPE
-      }
-
-      Agent.log('GETTING STATE', translatableTargetName)
-      const translatableTarget = await Agent.state(translatableTargetName)
-      Agent.log('GOT STATE', JSON.stringify(translatableTarget))
-
-      const { source_language } = itemState.translations
-
-      translatableTarget.source_language = source_language
-      translatableTarget.path = [id, ...path]
-      Agent.log('RESOLVING PATH', [...path])
-      const source_string = resolvePath([...path], itemState)
-      Agent.log('GOT SOURCE STRING', translatableTargetName, source_string)
-      if (source_string) {
-        translatableTarget.source_string = source_string
-        await setTranslation(
-          translatableTargetMetadata.id,
-          source_language,
-          source_string
-        )
-      }
-    }))
-  }
-
-  async function isTranslatableItem(id) {
-    const state = await Agent.state(id)
-    //  TODO: validate schema
-    return !!state.translations
-  }
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 5112,
+    port: 5444,
   },
   build: {
     target: 'esnext'
