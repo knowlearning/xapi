ef3c9e3d1dd2941f91a660a5a9a861095829c099
Matthew McClain <mthommcclain@gmail.com>
2025-06-16 10:44:41 -0700

    cleanup dashboard logic a bit

--- src/components/xapi-log.vue
+++ src/components/xapi-log.vue
@@ -9,28 +9,32 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, onBeforeUnmount } from 'vue'
 import XapiTable from './xapi-table.vue'
 
-const dumbdumb = 'f74e9cb3-2b53-4c85-9b0c-f1d61b032b3f'
-const mattie = 'c0f3a481-d4d5-4133-a198-94a325aa4536'
 
-const xapi = ref(null)
+const DUMBDUMB = 'f74e9cb3-2b53-4c85-9b0c-f1d61b032b3f'
+const MATTIE = 'c0f3a481-d4d5-4133-a198-94a325aa4536'
+const TEST_SEQUENCE = 'a331fae0-42de-11f0-a7f9-59378a7d0438'
+const TEST_EMBED_PATH = [ ]
+const TEST_USERS = [ MATTIE, DUMBDUMB]
 
-await pollXapi()
+const xapi = ref([])
+let polling = true
 
-async function pollXapi() {
-	xapi.value = await fetchXapi()
-	setTimeout(pollXapi, 2000)
+pollXapi(TEST_USERS, TEST_EMBED_PATH)
+
+async function pollXapi(users, empath) {
+	if (!polling) return
+	try { xapi.value = await fetchXapi(users, empath) }
+	catch (err) { console.error('Failed to fetch xAPI data:', err.message) }
+	setTimeout(() => pollXapi(users, empath), 2000)
 }
 
-async function fetchXapi(users = [mattie, dumbdumb], empath = []) {
-	console.log('fetching....')
-	return Agent.query('statements', [users, empath], 'xapi.knowlearning.systems').then(result => {
-		console.log('got statements....', result)
-		return result
-	})
+async function fetchXapi(users, empath) {
+	return await Agent.query('statements', [users, empath], 'xapi.knowlearning.systems')
 }
 
+onBeforeUnmount(() => { polling = false })
 
 </script>
